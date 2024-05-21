7dd129ff542886ce3b4f64feab44c5b5e3520c22
khh870220 <khh8702201@gmail.com>
2024-05-21 17:07:29 +0900

    20240521 전환가입 업데이트2

--- frm_Member/frmMember_Update.cs
+++ frm_Member/frmMember_Update.cs
@@ -3656,7 +3656,13 @@ namespace MLM_Program
 
             if (txtB1.Text.Trim() == "") txtB1.Text = "0";
             //20240521 구현호 태국은 US아이디 안받는다
-            //if (Check_USA_Error() == false) return;
+            
+            // 한국인 경우에만
+            if (cls_NationService.GetCountryCodeOrDefault(cls_User.gid_CountryCode) == "KR")
+            {
+                if (Check_USA_Error() == false) return;
+            }
+
             if (Check_TextBox_Error() == false) return;
 
             if (check_CC.Checked == true)
